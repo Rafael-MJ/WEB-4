ab6c164ec11df8e8d151445a75300045d992132c
JoseMRezende <rezendinhobr18@gmail.com>
2024-05-08 21:25:14 -0300

    refactor/curso

--- src/main/java/com/laranjeirosgroup/ac2/Models/Curso.java
+++ src/main/java/com/laranjeirosgroup/ac2/Models/Curso.java
@@ -1,4 +1,30 @@
 package com.laranjeirosgroup.ac2.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Curso")
 public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String descricao;
+    private String cargaHoraria;
+    private String objetivo;
+    private String ementa;
+
+    //Todo: Professor
+    //Todo: Agenda
 }
