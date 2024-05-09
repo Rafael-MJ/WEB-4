47c60e54cabe165c3091a89f09f7160985ef87d3
rafael-mj <rafaeljakubovsky@gmail.com>
2024-05-09 00:30:53 -0300

    feat: add base requests to 'Professor'

--- src/main/java/com/laranjeirosgroup/ac2/Controllers/ProfessorController.java
+++ src/main/java/com/laranjeirosgroup/ac2/Controllers/ProfessorController.java
@@ -1,9 +1,67 @@
 package com.laranjeirosgroup.ac2.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.laranjeirosgroup.ac2.Dtos.ProfessorDTO;
+import com.laranjeirosgroup.ac2.Models.Professor;
+import com.laranjeirosgroup.ac2.Services.ProfessorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/professores")
 public class ProfessorController {
+
+  @Autowired()
+  private ProfessorService professorService;
+
+  @PostMapping()
+  public ResponseEntity<Object> register(@RequestBody @Valid ProfessorDTO professorDTO) {
+    return ResponseEntity.status(HttpStatus.OK).body(professorService.registerProfessor(professorDTO));
+  }
+
+  @GetMapping()
+  public ResponseEntity<List<Professor>> getAllProfessores() {
+    return ResponseEntity.status(HttpStatus.OK).body(professorService.getAllProfessores());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getProfessorById(@PathVariable(value = "id") int professorId) {
+    Optional<Professor> professorModel = professorService.getProfessorById(professorId);
+
+    if (professorModel.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor id not found");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(professorModel);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateProfessorById(@RequestBody @Valid ProfessorDTO professorDto,
+    @PathVariable(value = "id") int professorId) {
+
+    Optional<Professor> professorModel = professorService.updateProfessorById(professorDto, professorId);
+
+    if (professorModel.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor id not found");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(professorModel);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteProfessorById(@PathVariable(value = "id") int professorId) {
+    Optional<Professor> professorModel = professorService.deleteProfessorById(professorId);
+
+    if (professorModel.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor id not found");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body("Professor deleted successfuly");
+  }
+
 }
--- src/main/java/com/laranjeirosgroup/ac2/Services/ProfessorService.java
+++ src/main/java/com/laranjeirosgroup/ac2/Services/ProfessorService.java
@@ -1,7 +1,63 @@
 package com.laranjeirosgroup.ac2.Services;
 
+import com.laranjeirosgroup.ac2.Dtos.ProfessorDTO;
+import com.laranjeirosgroup.ac2.Models.Professor;
+import com.laranjeirosgroup.ac2.Repositories.ProfessorRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfessorService {
+
+  @Autowired()
+  private ProfessorRepository professorRepository;
+
+  @Transactional()
+  public Professor registerProfessor(ProfessorDTO professorDTO) {
+    var newProfessor = new Professor();
+    BeanUtils.copyProperties(professorDTO, newProfessor);
+
+    return professorRepository.save(newProfessor);
+  }
+
+  public List<Professor> getAllProfessores() {
+    return professorRepository.findAll();
+  }
+
+  public Optional<Professor> getProfessorById(int professorId) {
+    return professorRepository.findById(professorId);
+  }
+
+  @Transactional()
+  public Optional<Professor> updateProfessorById(ProfessorDTO professorDto, int professorId) {
+    var professorModel = professorRepository.findById(professorId);
+
+    if (professorModel.isEmpty()) {
+      return Optional.empty();
+    }
+
+    var updatedProfessor = professorModel.get();
+    BeanUtils.copyProperties(professorDto, updatedProfessor);
+
+    return Optional.of(professorRepository.save(updatedProfessor));
+  }
+
+  @Transactional()
+  public Optional<Professor> deleteProfessorById(int professorId) {
+    var professorModel = professorRepository.findById(professorId);
+
+    if (professorModel.isEmpty()) {
+      return Optional.empty();
+    }
+
+    professorRepository.delete(professorModel.get());
+
+    return professorModel;
+  }
+
 }
