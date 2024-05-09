0235cfc968a1f83bb9564ff5ea67048508b8f917
Rafael-MJ <rafaeljakubovsky@gmail.com>
2024-05-08 22:01:20 -0300

    refactor: change id type

--- src/main/java/com/laranjeirosgroup/ac2/Repositories/ProfessorRepository.java
+++ src/main/java/com/laranjeirosgroup/ac2/Repositories/ProfessorRepository.java
@@ -4,8 +4,6 @@ import com.laranjeirosgroup.ac2.Models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
+public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 }
