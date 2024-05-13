08354c1753e3a347dcf1249bd25516b86210001b
Rafael-MJ <rafaeljakubovsky@gmail.com>
2024-05-13 19:34:53 -0300

    feat: add personalized methods to repositories

--- src/main/java/com/laranjeirosgroup/ac2/Repositories/AgendaRepository.java
+++ src/main/java/com/laranjeirosgroup/ac2/Repositories/AgendaRepository.java
@@ -4,6 +4,10 @@ import com.laranjeirosgroup.ac2.Models.Agenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface AgendaRepository extends JpaRepository<Agenda, Integer> {
+    List<Agenda> findByProfessorIdAndData(Long professorId, LocalDateTime data);
 }
--- src/main/java/com/laranjeirosgroup/ac2/Repositories/ProfessorRepository.java
+++ src/main/java/com/laranjeirosgroup/ac2/Repositories/ProfessorRepository.java
@@ -2,8 +2,16 @@ package com.laranjeirosgroup.ac2.Repositories;
 
 import com.laranjeirosgroup.ac2.Models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+    List<Professor> findByEspecializacao(String especializacao);
+
+    @Query("SELECT COUNT(a) FROM Agenda a WHERE a.professor.id = :professorId AND a.data = :data")
+    int countAgendasByProfessorAndDate(Long professorId, LocalDateTime data);
 }
