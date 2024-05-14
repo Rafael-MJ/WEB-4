2665bb3099e0d357cf9a091a5120d33871dc5e02
rafael-mj <rafaeljakubovsky@gmail.com>
2024-05-14 06:46:45 -0300

    fix: correct verificarDisponibilidade method

--- src/main/java/com/laranjeirosgroup/ac2/Utils/AgendaUtil.java
+++ src/main/java/com/laranjeirosgroup/ac2/Utils/AgendaUtil.java
@@ -7,8 +7,9 @@ public class AgendaUtil {
 
   public static boolean verificarDisponibilidade(List<LocalDateTime> dateTimeList, LocalDateTime targetDateTime) {
     for (LocalDateTime dateTime : dateTimeList) {
-      if (dateTime.equals(targetDateTime) ||
-        (dateTime.isAfter(targetDateTime) && dateTime.isBefore(targetDateTime.plusHours(1)))) {
+      if (dateTime.isEqual(targetDateTime) ||
+        (dateTime.isAfter(targetDateTime) && dateTime.isBefore(targetDateTime.plusHours(1))) ||
+        (dateTime.plusHours(1).isAfter(targetDateTime) && dateTime.plusHours(1).isBefore(targetDateTime.plusHours(2)))) {
         return true;
       }
     }
